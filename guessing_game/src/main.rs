b2ac921e67aeaf37955b64f15cc4630d27ebe2be
VectorTensor <prayashthapa15@gmail.com>
2025-11-15 20:48:37 +0545

    chapter 2 complete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,21 +8,33 @@ fn main(){
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
 
-    println!("Please input your guess. ");
+    loop {
+        println!("Please input your guess. ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("The secret number is {secret_number}");
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, 
+            Err(_) => break, 
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small !!"), 
-        Ordering::Greater => println!("Too big !!"),
-        Ordering::Equal => println!("You win!"),
+        };
+
+        println!("The secret number is {secret_number}");
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small !!"), 
+            Ordering::Greater => println!("Too big !!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
     }
 
 
